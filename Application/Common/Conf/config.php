d428809d3612c18f8ee64e2ad62db11af189bbb8
haiqing.lin <1047534821@qq.com>
2020-02-01 18:06:05 +0800

    a

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -16,7 +16,7 @@ return array(
     'DB_HOST'               =>  '127.0.0.1', // 服务器地址
     'DB_NAME'               =>  'cnmmmcom',          // 数据库名
     'DB_USER'               =>  'root',      // 用户名
-    'DB_PWD'                =>  '2e1cf461e540cfbb',          // 密码
+    'DB_PWD'                =>  'root',          // 密码
     'DB_PORT'               =>  '3306',        // 端口
     'DB_PREFIX'             =>  'wx_',    // 数据库表前缀
 );
